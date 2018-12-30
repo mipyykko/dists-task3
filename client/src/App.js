c08675bfabf57b96e23d7692f4e89634268d7513
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2018-12-30 22:57:49 +0200

    server can now get command with arguments, send signals; client can configure them

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,20 @@ import { 
   Button, 
   Card, CardContent, 
   Paper,
-  Radio, RadioGroup, 
   Typography, 
-  Grid
+  Grid,
+  TextField, Checkbox
 } from '@material-ui/core'
 
 const messaging = firebase.messaging()
 const API_URL = 'http://localhost:8082'
 
 const states = {
-  OK: 'OK',
   ERROR: 'ERROR',
   REGISTERING: 'REGISTERING',
   UNREGISTERING: 'UNREGISTERING',
-  CHANGING_MODE: 'CHANGING_MODE'
-}
-
-const modes = {
-  MIN: 'MIN',
-  AVG: 'AVG',
-  MAX: 'MAX',
-  RANDOM: 'RANDOM'
+  STARTING: 'STARTING',
+  STOPPING: 'STOPPING'
 }
 
 class App extends Component {
@@ -36,7 +29,10 @@ class App extends Component {
     this.state = {
       token: null,
       state: states.OK,
-      mode: modes.RANDOM,
+      receiving: false,
+      payloadLength: 5,
+      messageAmount: 50,
+      uniquePayloads: true,
       error: null,
       messages: []
     }
@@ -51,16 +47,6 @@ class App extends Component {
       ) / this.state.messages.length).toFixed(2)
   }
 
-  apiPost = (endpoint, data) => {
-    return fetch(`${API_URL}/${endpoint}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-  }
-
   newMessage = (msg) => (
     {
       title: msg.notification.title,
@@ -71,89 +57,139 @@ class App extends Component {
   )
 
   handleMessage = (msg) => {
+    const { signal } = msg.data
+
+    if (signal) {
+      switch (signal) {
+        case 'STOP':
+          this.setState({ ...this.state, receiving: false })
+          break
+        default:
+      }
+
+      return
+    } 
+    
     this.setState({
       ...this.state,
-      messages: this.state.messages.concat(this.newMessage(msg))
+      messages: this.state.messages.concat(this.newMessage(msg)),
     })
   }
 
-  handleTokenClick = () => {
-    messaging.onMessage(this.handleMessage)
-    messaging
-      .requestPermission()
-      .then(() => messaging.getToken())
-      .then(token => {
-        if (token) {
-          this.setState({ ...this.state, token })
-        } else {
-          console.log("error retrieving token")
+  handlePost = (options) => {
+    const {
+      endpoint,
+      data,
+      enterState,
+      okValues = {},
+      errorValues = {},
+      okState = states.OK,
+      errorState = states.ERROR
+    } = options
+
+    this.setState({ ...this.state, state: enterState, error: null })
+
+    fetch(`${API_URL}/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => {
+        console.log(res)
+        this.setState({ ...this.state, ...okValues, state: okState })
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({ ...this.state, ...errorValues, state: errorState })
+      })
+  }
+
+  handleClick = (btn) => () => {
+    var options = {}
+
+    switch (btn) {
+      case 'register':
+      case 'unregister':
+        options = {
+          endpoint: btn,
+          data: { token: this.state.token }, 
+          enterState: btn === 'register' ? states.REGISTERING : states.UNREGISTERING,
+          okValues: btn === 'register' ? { registered: true } : { registered: false, token: null }
         }
-      })
-      .catch(err => console.log(err))
-  }
+        this.handlePost(options)
 
-  handleRegisterClick = () => {
-    this.setState({ ...this.state, state: states.REGISTERING, error: null })
+        break
+      case 'start':
+        options = {
+          endpoint: 'start',
+          data: { 
+            amount: this.state.messageAmount, 
+            length: this.state.payloadLength, 
+            unique: this.state.uniquePayloads
+          },
+          enterState: states.STARTING, 
+          okValues: { receiving: true, messages: [] },
+          errorValues: { receiving: false }
+        }
+        this.handlePost(options)
 
-    this.apiPost('register', { token: this.state.token })
-      .then(res => {
-        console.log(res)
-        this.setState({ ...this.state, registered: true, state: states.OK })
-      })
-      .catch(err => {
-        this.setState({ ...this.state, state: states.ERROR, error: "There was a problem registering." })
-        console.log(err)
-      })
-  }
-
-  handleUnregisterClick = () => {
-    this.setState({ ...this.state, state: states.UNREGISTERING, error: null })
-
-    this.apiPost('unregister', { token: this.state.token })
-      .then(res => {
-        console.log(res)
-        this.setState({ ...this.state, token: null, registered: false, state: states.OK })
-      })
-      .catch(err => {
-        console.log(err)
-        this.setState({ ...this.state, state: states.ERROR, error: "There was a problem unregistering." })
-      })
-  }
-
-  handleModeChange = (event, value) => {
-    this.setState({ ...this.state, state: states.CHANGING_MODE, error: null })
-
-    this.apiPost('payload', { mode: value })
-      .then(res => {
-        console.log(res)
-        this.setState({ ...this.state, mode: value, state: states.OK })
-      })
-      .catch(err => {
-        console.log(err)
-        this.setState({ ...this.state, error: "There was a problem changing the mode.", state: states.ERROR })
-      })
+        break
+      case 'token':
+        messaging.onMessage(this.handleMessage)
+        messaging
+          .requestPermission()
+          .then(() => messaging.getToken())
+          .then(token => {
+            if (token) {
+              this.setState({ ...this.state, token })
+            } else {
+              console.log("error retrieving token")
+            }
+          })
+          .catch(err => console.log(err))
+        break
+      default:
+    }
   }
 
   handleReset = () => {
     this.setState({ ...this.state, messages: [] })
   }
 
-  renderModes = () => (
+  handleChange = (field) => (event) => {
+    this.setState({ ...this.state, [field]: event.target.value })
+  }
+
+  handleCheckbox = (field) => (event) => {
+    this.setState({ ...this.state, [field]: event.target.checked })
+  }
+
+  renderForm = () => (
     <div>
-      <RadioGroup 
-        name="mode" 
-        defaultSelected={this.state.mode}
-        onChange={this.handleModeChange}
-        disabled={this.state === states.CHANGING_MODE}
-      >
-        {Object.keys(modes).map(mode => (
-          <Radio
-            value={mode}
-            label={mode}
-            key={mode}
-          />
-        ))}
-      </RadioGroup>
+      <form noValidate autoComplete='off'>
+        <TextField
+          id="length"
+          label="Payload length"
+          value={this.state.payloadLength}
+          type="number"
+          onChange={this.handleChange('payloadLength')}
+        />
+        <TextField
+          id="count"
+          label="Message amount"
+          value={this.state.messageAmount}
+          type="number"
+          onChange={this.handleChange('messageAmount')}
+        />
+        <Checkbox
+          id="unique"
+          label="Unique payloads"
+          checked={this.state.uniquePayloads}
+          onChange={this.handleCheckbox('uniquePayloads')}
+        />
+      </form>
     </div>  
   )
 
@@ -177,27 +213,28 @@ class App extends Component {
               <p>Currently using token {this.state.token}</p>
             : <div>
                 <p>Click on the button below to get a token. Remember to allow notifications!</p>
-                <Button variant="raised" onClick={this.handleTokenClick}>Get token</Button>
+                <Button variant="raised" onClick={this.handleClick('token')}>Get token</Button>
               </div>
             }
           </Grid>
           {this.state.token && !this.state.registered ?
             <div>
               <p>Click to register your client at the server.</p>
-              <Button variant="raised" disabled={this.state.state === states.REGISTERING} onClick={this.handleRegisterClick}>Register</Button>
+              <Button variant="raised" disabled={this.state.state === states.REGISTERING} onClick={this.handleClick('register')}>Register</Button>
             </div>
           : this.state.registered 
             ? <div>
                 <p>Registered to server. Click to unregister.</p>
-                <Button variant="raised" disabled={this.state.state === states.UNREGISTERING} onClick={this.handleUnregisterClick}>Unregister</Button>
+                <Button variant="raised" disabled={this.state.state === states.UNREGISTERING} onClick={this.handleClick('unregister')}>Unregister</Button>
               </div> 
             : null
           }
           {this.state.error ? <Typography color="error">{this.state.error}</Typography> : null}
-          {this.state.registered && this.renderModes()}
+          {this.state.registered && this.renderForm()}
+          {!this.state.receiving && this.state.registered ? <Button variant="raised" disabled={this.state.state === states.STARTING} onClick={this.handleClick('start')}>Start</Button> : null}
           <p>{this.state.messages.length} message{this.state.messages.length === 1 ? '' : 's'} received, average time {this.averageTime()} ms</p>
           <Paper style={{maxHeight: 600, overflow: 'auto' }}>
-            {this.state.messages.map(msg => this.renderMessage(msg))}
+            {this.state.messages.slice(-10).map(msg => this.renderMessage(msg))}
           </Paper>
         </Grid>
       </div>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var fcm = require('firebase-admin')
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var serviceAccount = require('./serviceaccount.json')
+var loremIpsum = require('lorem-ipsum')
 
 var app = express()
 
@@ -14,36 +15,22 @@ fcm.initializeApp({
   databaseURL: 'https://dists-task3.firebaseio.com'
 })
 
-var sendingMessages = true
+var sendingMessages = false
 var messageIdx = 0
+var sentCount = 0
+var toBeSentCount = 50
 var registeredClients = []
+var delay = 100
+var delayRandom = 50
+var payloadLength = 5
+var uniquePayloads = true
+let payload = ''
 
-const payloadModes = {
-  MIN: 'MIN',
-  MAX: 'MAX',
-  AVG: 'AVG',
-  RANDOM: 'RANDOM'
-}
+const getPayload = () => payload || loremIpsum({ count: payloadLength, units: 'sentences' })
+const getDelay = () => delay + Math.random() * delayRandom
 
-const payloads = {
-  MIN: 'Lorem ipsum dolor sit amet.',
-  AVG: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu consectetur dui, a condimentum nisl. Morbi volutpat tempor erat eu finibus. Morbi aliquam massa eu tellus iaculis, nec mollis est posuere. Integer feugiat velit sed metus cursus euismod. Ut nec magna ipsum. Nulla sem dolor, varius vitae congue sed, rhoncus.',
-  MAX: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id mi non lorem tempor eleifend. Fusce vel lobortis eros. Nunc et ex mauris. Quisque pharetra nunc ut faucibus elementum. Sed vel lorem non odio euismod pulvinar. Ut sodales augue tortor, ut hendrerit erat ultrices sed. Morbi quis blandit lectus. Praesent aliquet augue vitae ligula ultricies laoreet ac sit amet dolor. Maecenas in dignissim neque. Quisque tincidunt dui vitae ipsum ullamcorper ultrices fermentum a tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam fermentum massa in risus lobortis egestas. Donec nec dictum purus. Pellentesque arcu mi, imperdiet vel urna at, faucibus viverra ipsum. Sed aliquam risus tellus, quis aliquet enim rhoncus id. Donec sodales aliquam urna. Nullam tempor gravida sapien non auctor. Curabitur sed lacus ultrices, sodales ex nec, venenatis massa. Proin et laoreet orci, ut finibus erat. Nam ante leo, vehicula ac felis sed, venenatis blandit metus. Praesent at risus sed ex aliquet feugiat. Suspendisse vitae est quis lacus fermentum hendrerit non quis risus. Cras imperdiet eros in mattis efficitur. Maecenas luctus magna ut dictum finibus. Maecenas aliquet ante eget erat faucibus, sit amet pharetra dolor consequat. In quis sapien sed justo venenatis rutrum. Fusce lorem nunc, rhoncus ut massa sed, accumsan consectetur lectus. In ullamcorper tristique justo in cursus. Nam sed purus orci. Nullam vitae imperdiet ipsum. Suspendisse vulputate nibh et ligula consequat, non ullamcorper mi accumsan. Vivamus nec molestie justo, quis scelerisque justo. Duis eu facilisis nunc. Vivamus molestie purus eu metus bibendum molestie. Integer tempor mi ac ligula viverra, ut aliquet urna tempor. Vestibulum porttitor diam turpis, vitae auctor leo tincidunt id. Suspendisse a lacus lorem. Aliquam nec tellus id eros maximus tincidunt. Nam vitae odio fermentum, feugiat velit ut, facilisis tellus. Sed tincidunt malesuada metus vitae cursus. Donec viverra porta justo, id eleifend enim laoreet ut. Nulla vitae sapien elit. Aliquam ex magna, fermentum vitae eleifend a, ultricies sed elit. Nulla interdum quam id libero vulputate ornare vitae tempus risus. Nunc ut ultricies dolor. Quisque commodo ex a bibendum sagittis. Vestibulum eget lacinia tortor, porta bibendum mi. Donec in dui consequat lectus consequat varius vel et nibh. Proin varius nec elit eu tincidunt. Integer quam odio, porta sit amet pharetra vitae, viverra placerat nibh. Nullam ac mi tempus, elementum nulla sit amet, efficitur risus. Nam eleifend a leo eget bibendum. Vestibulum maximus justo ac dolor sagittis egestas. Donec quis semper magna. Nulla suscipit enim sodales nunc tempus, tincidunt congue urna euismod. Ut lacinia gravida lorem at varius. Maecenas et lacus posuere, sollicitudin erat eget, scelerisque dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus fermentum molestie. Suspendisse rutrum, leo eu gravida venenatis, mi mauris varius erat, eu varius enim justo eget nisl. Vestibulum imperdiet mollis pellentesque. Vivamus et porta dolor. Sed massa metus, faucibus a consequat vel, efficitur nec tortor. Etiam et lectus consequat, rhoncus massa vel, fermentum nisl. Curabitur laoreet pulvinar vulputate. Phasellus non est sapien. Nulla egestas dui eget sapien ullamcorper sagittis. Nam vitae nunc tristique.' 
-}
-
-var payloadMode = payloadModes.RANDOM
-
-const getPayload = () => {
-  switch (payloadMode) {
-    case payloadModes.MIN:
-    case payloadModes.MAX:
-    case payloadModes.AVG:
-      return payloads[payloadMode]
-    case payloadModes.RANDOM:
-      return payloads[Object.keys(payloads)[Math.floor(Math.random() * Object.keys(payloads).length)]]
-    default:
-      return 'unknown payload mode'
-  }
+const scheduleSending = () => {
+  setTimeout(sendMessage, getDelay())
 }
 
 const sendMessage = () => {
@@ -75,22 +62,47 @@ const sendMessage = () => {
     })
   }
 
-  if (sendingMessages) {
-    setTimeout(sendMessage, 1000 + Math.random() * 500)
+  if (++sentCount < toBeSentCount) {
+    scheduleSending()
+  } else {
+    sendingMessages = false
+    sendStop()
   }
+}
+
+const sendStop = () => {
+  console.log("sent stop signal")
+
+  registeredClients.forEach(token => {
+    const message = {
+      'notification': { 'body': 'asdf' },
+      'data': { 'signal': 'STOP' }, 
+      'token': token
+    }
+
+    fcm
+      .messaging()
+      .send(message)
+  })
 }
 
 app.get("/", (req, res) => {
   res.send("hello")
 })
 
-app.post("/payload", (req, res) => {
-  const { mode } = req.body
+app.post("/start", async (req, res) => {
+  const { length, amount, unique = true } = req.body
 
-  if (mode && payloadModes[mode]) {
-    payloadMode = payloadModes[mode]
-    console.log('Payload mode changed to', mode)
-    
+  if (!sendingMessages && length && amount)  {
+    console.log("starting sending")
+    sentCount = 0
+    toBeSentCount = amount
+    payloadLength = length
+    sendingMessages = true
+    payload = unique ? '' : loremIpsum({ count: payloadLength, units: 'sentences' })
+
+    scheduleSending()
+
     res.sendStatus(200)
   } else {
     res.sendStatus(500)
@@ -104,9 +116,9 @@ app.post("/register", (req, res) => {
     if (!registeredClients.includes(token)) {
       registeredClients.push(token)
       console.log("Registered new client: ", token)
-      
-      res.sendStatus(200)
-    }     
+    }      
+    
+    res.sendStatus(200)
   } else {
     res.sendStatus(500)
   } 
@@ -129,8 +141,6 @@ var server = app.listen(8082, () => {
   var host = server.address().address
   var port = server.address().port
   console.log(`Listening at port ${port}`)
-
-  setTimeout(sendMessage, 1000 + Math.random() * 500)
 })
 
 
